a5b9912f9433ae4ccc0d41f65ec3d36fe757b8e4
Camarones <cameron_walther@yahoo.com>
2019-02-10 16:34:24 -0500

    added a bit of depth to my logic, you click the crystal and it adds on top of a variable and lets you know in the log that it's working, and it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,10 @@
-// Fire off a test in the log to make sure it's clicking
+//make a variable to keep track of the number of times the image is clicked
+var counter = 0;
+
+// on the click of the crystal image
 $(".crystal-image").on("click", function() {
-    console.log("You have clicked a crystal");
+        // increase the counter by one when it's clicked
+    counter += 1;
+    // register a test in the log to make sure it's working
+    console.log("You have clicked a crystal and your score is " + counter);
 });
