4336f733599fe7027b13e8d0fc5bfbc87c6dfc6f
Camarones <cameron_walther@yahoo.com>
2019-02-11 11:49:56 -0500

    getting there, randomized the score, added all the images even though they're DRY for right now, basically there just needs logic and differentiating values for the images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,13 @@
 //make a variable to keep track of the number of times the image is clicked
 var counter = 0;
-
+// create a random number to target between 19 and 120
+var targetNumber = Math.floor(Math.random() * 120 + 19);
 // on the click of the crystal image
 $(".crystal-image").on("click", function() {
         // increase the counter by one when it's clicked
     counter += 1;
-    // register a test in the log to make sure it's working
-    console.log("You have clicked a crystal and your score is " + counter);
+    // we don't need this test in the log anymore, so we switch it to display the counter - which eventually will need to be a more complex variable
+    $("#currentScore").html(counter);
+    // show the target number in the target-display div
+    $("#target-display").html(targetNumber);
 });
